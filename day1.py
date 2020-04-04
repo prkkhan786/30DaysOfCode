e4920ee036d7784ba96d394057e419fc65aa291c
prkkhan786 <prkkhan1@gmail.com>
2020-04-05 01:36:55 +0530

    question three

--- day1.py
+++ day1.py
@@ -86,6 +86,33 @@ class Solutiion:
 
 
 
+    def maxSubArray(self,nums):
+        '''  Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+            Example:
+            Input: [-2,1,-3,4,-1,2,1,-5,4],
+            Output: 6
+            Explanation: [4,-1,2,1] has the largest sum = 6.
+        '''
+        currentSum = nums[0]
+        overallSum = nums[0]
+
+        for i in range(1,len(nums)):
+
+            if(currentSum<0):
+                currentSum = nums[i]
+            else:
+                currentSum = currentSum + nums[i]
+
+            if(overallSum<currentSum):
+                overallSum = currentSum
+        return overallSum
 
 
+
+
+
+
+c = Solutiion()
+print(c.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 

61d815c1673887a94fcdcfa0856f5847e6b5dac8
prkkhan786 <prkkhan1@gmail.com>
2020-04-05 22:18:01 +0530

    question four

--- day1.py
+++ day1.py
@@ -108,11 +108,34 @@ class Solutiion:
                 overallSum = currentSum
         return overallSum
 
+    def moveZeroes(self, nums):
+        '''Given an array nums, write a function to move all 0's to the end of it
+         while maintaining the relative order of the non-zero elements.
+        Example:
+        Input: [0,1,0,3,12]
+        Output: [1,3,12,0,0]
+        '''
+
+        index = 0
+        for i in range(len(nums)):
+            if(nums[i]!=0):
+                nums[index] = nums[i]
+                index = index +  1
+        for i in range(index,len(nums)):
+            nums[i] = 0
+        return nums
+
+
+
+
+
+
+
 
 
 
 
 
 c = Solutiion()
-print(c.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(c.moveZeroes([0,1,0,3,12]))
 
